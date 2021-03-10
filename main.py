302ee3613abac222f2e29f1e475dc3bd611cb2ab
luca farah <luca@ambientedecriacao.com.br>
2021-03-09 21:56:51 -0300

    final v1.0

--- main.py
+++ main.py
@@ -1,30 +1,102 @@
+import re
 import sys
 
-def calculadora(sys):
-    comando = sys.argv[1]
-    #Tira espaços da conta
-    comando = comando.replace(' ', '')    
-    numbers = ["0"]
-    operacao = []
-    cont_num = 0
-    for i in comando:
-        if (i == "+"):
-            operacao.append(0)
-            numbers.append("0")
-            cont_num += 1
-        elif(i == "-"):
-            operacao.append(1)
-            numbers.append("0")
-            cont_num += 1
+
+def tToken_finder(char):
+        if (char == "+"):
+            return "SUM"
+        elif(char == "-"):
+            return "MIN"
         else:
-            numbers[cont_num] += i
-    resultado = int(numbers[0])
-    for i in range(len(operacao)):
-        if operacao[i] == 0:
-            resultado += int(numbers[i+1])
-        elif operacao[i] == 1:
-            resultado -= int(numbers[i+1])
-    print("O resultado dessa operação é {0}".format(resultado))
+            return "NUM"
 
 
-calculadora(sys)
+class Token:
+  def __init__(self, tToken = None, value = None):
+    self.type = tToken
+    self.value = value
+
+
+
+class Tokenizer:
+  def __init__(self, origin = None, position = 0 ,actual = None):
+    self.origin = origin #string
+    self.position = position #posicao atual
+    self.actual = actual #token
+
+  def selectNext(self):
+        #se for o ultimo acaba
+        if (self.position >= len(self.origin)):
+            self.actual = Token(tToken= "END")
+            return self.actual
+        if(self.position < len(self.origin)):
+            #passa enquanto for espaço
+            # print("LEN {}".format(self.origin))
+            while(self.origin[self.position] == " "):
+                self.position += 1
+                #se for o ultimo acaba
+                if (self.position >= len(self.origin)):
+                    self.actual = Token(tToken= "END")
+                    return self.actual
+            #Aqui achou o proximo token valido.
+            
+            self.actual = Token(tToken= tToken_finder(self.origin[self.position]),value = self.origin[self.position])
+            self.position += 1
+        return self.actual
+
+        
+
+
+class Parser():
+    def __init__(self):
+        self.tokens = Tokenizer
+
+    @staticmethod
+    def parseExpression():
+        Parser.tokens.selectNext()
+        op = 0
+        while(Parser.tokens.actual.type != "END"):
+            numAtual = ""
+            if(Parser.tokens.actual.type == "NUM"):
+                while(Parser.tokens.actual.type == "NUM"):
+                    numAtual += Parser.tokens.actual.value
+                    Parser.tokens.selectNext()
+                if op == 0:
+                    resultado  = int(numAtual)
+                elif op == 1:
+                    resultado -= int(numAtual)
+                elif op == 2:
+                    resultado += int(numAtual)
+                
+                if (Parser.tokens.actual.type == "MIN"):
+                    op = 1
+                    Parser.tokens.selectNext()
+                elif(Parser.tokens.actual.type == "SUM"):
+                    op = 2
+                    Parser.tokens.selectNext()
+
+            else:
+                raise Exception ("Comando errado")
+        return resultado
+
+
+        
+    @staticmethod
+    def run(code):
+        Parser.tokens = Parser().tokens(origin = code) 
+        return Parser().parseExpression()
+    
+
+        
+
+def main():
+    if(len(sys.argv) < 2):
+        raise Exception("Comando não passado")
+    comando = ""
+    for i in range(1,len(sys.argv)):
+        comando += sys.argv[i]
+    print(Parser().run(comando))
+
+
+if __name__ == "__main__":
+    main()
