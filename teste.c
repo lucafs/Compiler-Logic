f74dd012ab9eb2ae943a7bbab12e9dc86609c06e
luca farah <luca@ambientedecriacao.com.br>
2021-05-11 11:29:02 -0300

    not working

--- teste.c
+++ teste.c
@@ -1,5 +1,10 @@
-x1 = 3; /* bla bla $x1 = 9999998 */
+{x1 = 3; /* bla bla $x1 = 9999998 */
 y2 = 4;
-x = readln() + 2;
+x = 1 + 2;
 z_final = x1 + y2 *33;
+x = 2;
+while (x && 4){
 println(x);
+x = x+1;}
+
+println(2);}
